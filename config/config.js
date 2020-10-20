328e07b5b1db21feb2c151d5a1e0c5a0e928851e
justinyuta <justin.yuta@gmail.com>
2020-10-20 11:11:37 +0800

    Update db connection to be process's env variable

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-# Add by VS 
+// Update by VS 2020/10/19 change to procee env variable 
 module.exports = {
     database: {
-        user: 'optoma',
-        password: 'Ab-1234567890',
-        connectString: 'DB19C_LOW'
+        user: process.env.HR_USER,
+        password: process.env.HR_PASSWORD,
+        connectString: process.env.HR_CONNECTIONSTRING
     },
     jwtSecretKey: "jmvhDdDBMvqb=M@6h&QVA7x"
 };
