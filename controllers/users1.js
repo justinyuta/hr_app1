537af1fce511000507a4bba771ce53894310c49d
justinyuta <justin.yuta@gmail.com>
2020-10-20 17:57:54 +0800

    Commit

--- controllers/users1.js
+++ controllers/users1.js
@@ -10,71 +10,65 @@ async function post(req, res, next) {
     };
 
     var unhashedPassword = req.body.password;
- 
-    bcrypt.genSalt(10, function(err, salt) {
+    const insert_sql = 
+    'insert into jsao_users ( ' +
+    '   email, ' +
+    '   password, ' +
+    '   role ' +
+    ') ' +
+    'values (' +
+    '    :email, ' +
+    '    :password, ' +
+    '    \'BASE\' ' +
+    ') ' +
+    'returning ' +
+    '   id, ' +
+    '   email, ' +
+    '   role ' +
+    'into ' +
+    '   :rid, ' +
+    '   :remail, ' +
+    '   :rrole';
+    
+    const binds =  {
+        email: user.email.toLowerCase(),
+        password: user.hashedPassword,
+        rid: {
+            type: oracledb.NUMBER,
+            dir: oracledb.BIND_OUT
+        },
+        remail: {
+            type: oracledb.STRING,
+            dir: oracledb.BIND_OUT
+        },
+        rrole: {
+            type: oracledb.STRING,
+            dir: oracledb.BIND_OUT
+        }
+        };
+
+    opts = { autoCommit: true};
+
+    const salt = bcrypt.genSalt(10, function(err, salt) {
         if (err) {
             return next(err);
         }
- 
-        bcrypt.hash(unhashedPassword, salt, function(err, hash) {
-            if (err) {
-                return next(err);
-            }
-            user.hashedPassword = hash;             
-
-            const insert_sql = 
-            'insert into jsao_users ( ' +
-            '   email, ' +
-            '   password, ' +
-            '   role ' +
-            ') ' +
-            'values (' +
-            '    :email, ' +
-            '    :password, ' +
-            '    \'BASE\' ' +
-            ') ' +
-            'returning ' +
-            '   id, ' +
-            '   email, ' +
-            '   role ' +
-            'into ' +
-            '   :rid, ' +
-            '   :remail, ' +
-            '   :rrole';
-            
-            const binds =  {
-                email: user.email.toLowerCase(),
-                password: user.hashedPassword,
-                rid: {
-                    type: oracledb.NUMBER,
-                    dir: oracledb.BIND_OUT
-                },
-                remail: {
-                    type: oracledb.STRING,
-                    dir: oracledb.BIND_OUT
-                },
-                rrole: {
-                    type: oracledb.STRING,
-                    dir: oracledb.BIND_OUT
-                }
-                };
-    
-            opts = { autoCommit: true};
-            try {
-                const results = await database.simpleExecute(insert_sql, binds, opts);
-                user.role = 'BASE';
-                res.status(200).json({
-                    user: user
-                });
-            } catch (err) {
-                return next(err);
-            }        
-        });
+    }); 
+    user.hashedPassword = bcrypt.hash(unhashedPassword, salt, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
     });
-
-
-
-  }
+    try {
+        const results = database.simpleExecute(insert_sql, binds, opts);
+        user.role = 'BASE';
+        res.status(200).json({
+            user: user
+        });
+    } catch (err) {
+        return next(err);
+    }        
+}
    
   module.exports.post = post;
   
