db5ede96f0ee82804ef545b1fee7c3f8bc4295bf
justinyuta <justin.yuta@gmail.com>
2020-10-20 17:43:05 +0800

    Commit

--- controllers/users1.js
+++ controllers/users1.js
@@ -11,12 +11,12 @@ async function post(req, res, next) {
 
     var unhashedPassword = req.body.password;
  
-    bcrypt.genSalt(10, function(err, salt) {
+    await bcrypt.genSalt(10, function(err, salt) {
         if (err) {
             return next(err);
         }
  
-        bcrypt.hash(unhashedPassword, salt, function(err, hash) {
+        await bcrypt.hash(unhashedPassword, salt, function(err, hash) {
             if (err) {
                 return next(err);
             }
