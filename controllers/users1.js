4df9ec728479440e9d928fe6c878cdb8de61dee1
Justin Hsu <justin.yuta@gmail.com>
2020-10-26 07:43:04 +0000

    Commit - user1 for test ssampleExecute

--- controllers/users1.js
+++ controllers/users1.js
@@ -8,46 +8,10 @@ async function post(req, res, next) {
         email: req.body.email,
         password: req.body.password
     };
+//    console.log('req.body:');
+//    console.log(req.body);
 
     var unhashedPassword = req.body.password;
-    const insert_sql = 
-    'insert into jsao_users ( ' +
-    '   email, ' +
-    '   password, ' +
-    '   role ' +
-    ') ' +
-    'values (' +
-    '    :email, ' +
-    '    :password, ' +
-    '    \'BASE\' ' +
-    ') ' +
-    'returning ' +
-    '   id, ' +
-    '   email, ' +
-    '   role ' +
-    'into ' +
-    '   :rid, ' +
-    '   :remail, ' +
-    '   :rrole';
-    
-    const binds =  {
-        email: user.email.toLowerCase(),
-        password: user.hashedPassword,
-        rid: {
-            type: oracledb.NUMBER,
-            dir: oracledb.BIND_OUT
-        },
-        remail: {
-            type: oracledb.STRING,
-            dir: oracledb.BIND_OUT
-        },
-        rrole: {
-            type: oracledb.STRING,
-            dir: oracledb.BIND_OUT
-        }
-        };
-
-    opts = { autoCommit: true};
 
     const salt = await bcrypt.genSalt(10, function(err, salt) {
         if (err) {
@@ -57,25 +21,69 @@ async function post(req, res, next) {
             if (err) {
                 return next(err);
             }
+
             user.hashedPassword = hash;
-            console.log('hashedPassword:');
-            console.log(user);        
+
+//            console.log('hashedPassword:');
+//            console.log(user);  
+
+            const insert_sql = 
+            'insert into jsao_users ( ' +
+            '   email, ' +
+            '   password, ' +
+            '   role ' +
+            ') ' +
+            'values (' +
+            '    :email, ' +
+            '    :password, ' +
+            '    \'BASE\' ' +
+            ') ' +
+            'returning ' +
+            '   id, ' +
+            '   email, ' +
+            '   role ' +
+            'into ' +
+            '   :rid, ' +
+            '   :remail, ' +
+            '   :rrole';
+            
+            const binds =  {
+                email: user.email.toLowerCase(),
+                password: user.hashedPassword,
+                rid: {
+                    type: oracledb.NUMBER,
+                    dir: oracledb.BIND_OUT
+                },
+                remail: {
+                    type: oracledb.STRING,
+                    dir: oracledb.BIND_OUT
+                },
+                rrole: {
+                    type: oracledb.STRING,
+                    dir: oracledb.BIND_OUT
+                }
+                };
+        
+            opts = { autoCommit: true};
+        
+//            console.log('Binds:');
+//            console.log(binds);
+        
+            const results = database.simpleExecute(insert_sql, binds, opts).then(function(results){
+                console.log('Result:');
+                console.log(results);
+                user.role = 'BASE';
+                res.status(200).json({
+                    user: user
+                });
+            }, function(err){
+                console.log('Database execute error :');
+                console.log(err);
+                return next(err);
+            });        
         });
     }); 
-    console.log('User:');
-    console.log(user);
 
-    
-    const results = await database.simpleExecute(insert_sql, binds, opts).then(function(results){
-        console.log('Result:');
-        console.log(results);
-        user.role = 'BASE';
-        res.status(200).json({
-            user: user
-        });
-    }, function(err){
-        return next(err);
-    });
 }
    
   module.exports.post = post;
